37a4ee07dbf896a33eb2dd65993903682f8f7721
Robin A. Meade <robin.a.meade@gmail.com>
2017-09-17 00:14:02 +0000

    Expanded .zip file

---
+++ src/main/java/com/example/corejava/CoreJavaApplication.java
@@ -0,0 +1,12 @@
+package com.example.corejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreJavaApplication.class, args);
+	}
+}
